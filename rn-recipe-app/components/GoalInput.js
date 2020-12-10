c288b108a48ee0c38297a534426fc45c2bb046e2
jperomingan@startechup.com <jperomingan@startechup.com>
2020-12-10 19:28:33 +0800

    More Flexbox Styling

--- rn-recipe-app/components/GoalInput.js
+++ rn-recipe-app/components/GoalInput.js
@@ -1,33 +1,38 @@
 import React, { useState } from 'react';
-import { TextInput, View, Button, StyleSheet } from 'react-native';
+import { TextInput, View, Button, StyleSheet, Modal } from 'react-native';
 
 const GoalInput = props => {
     const [enteredGoal, setEnteredGoal] = useState('');
     const goalInputHandler = (enteredText) => {
         setEnteredGoal(enteredText);
       };
-    return (<View style={styles.inputContainer}>
-        <TextInput 
-            placeholder="Course Goal"
-            style={styles.input}
-            onChangeText={goalInputHandler}
-            value={enteredGoal}
-        />
-        <Button title="ADD" onPress={props.onAddGoal.bind(this, enteredGoal)}/>
-    </View>);
+    return (
+        <Modal visible={props.visible} animationType="slide">
+        <View style={styles.inputContainer}>
+            <TextInput 
+             placeholder="Course Goal"
+             style={styles.input}
+             onChangeText={goalInputHandler}
+             value={enteredGoal}
+            />
+            <Button title="ADD" onPress={props.onAddGoal.bind(this, enteredGoal)}/>
+        </View>
+    </Modal>
+    );
 };
 
 const styles = StyleSheet.create({
     inputContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
+        flex: 1,
+        justifyContent: 'center',
         alignItems: 'center'
       },
       input: {
         width: '80%',
         borderColor: 'black',
         borderWidth: 1,
-        padding: 10
+        padding: 10,
+        marginBottom: 10
       },
 });
 
