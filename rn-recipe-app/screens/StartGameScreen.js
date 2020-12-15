cf7c359a949d5eb1f6b1114def77ff879773d669
jperomingan@startechup.com <jperomingan@startechup.com>
2020-12-15 17:54:42 +0800

    Input text align center

--- rn-recipe-app/screens/StartGameScreen.js
+++ rn-recipe-app/screens/StartGameScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 import Card from '../components/Card';
 import Input from '../components/Input';
@@ -11,7 +11,7 @@ const StartGameScreen = props => {
             <Text style={styles.title}>Start a New Game!</Text>
             <Card style={styles.inputContainer}>
                 <Text>Select a Number</Text>
-                <TextInput />
+                <Input style={styles.input}/>
                 <View style={styles.buttonContainer}>
                    <View style={styles.button}><Button title="Reset" onPress={() => {}} color={Colors.accent}/></View>
                    <View style={styles.button}><Button title="Confirm" onPress={() => {}} color={Colors.primary}/></View>
@@ -44,6 +44,10 @@ const styles = StyleSheet.create({
     },
     button: {
         width: 100
+    },
+    input: {
+        width: 50,
+        textAlign: 'center'
     }
 })
 
