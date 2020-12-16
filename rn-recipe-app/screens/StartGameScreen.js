b510874bb54a8170c99a62468b22490b24a6700f
jperomingan@startechup.com <jperomingan@startechup.com>
2020-12-16 13:57:38 +0800

    Showing an Alert

--- rn-recipe-app/screens/StartGameScreen.js
+++ rn-recipe-app/screens/StartGameScreen.js
@@ -1,5 +1,13 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { 
+    StyleSheet, 
+    Text, 
+    View, 
+    Button, 
+    TouchableWithoutFeedback, 
+    Keyboard,
+    Alert 
+} from 'react-native';
 
 import Card from '../components/Card';
 import Input from '../components/Input';
@@ -19,7 +27,10 @@ const StartGameScreen = props => {
     }
     const confirmInputHandler = () => {
         const chosenNumber = parseInt(enteredValue);
-        if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99) {
+        if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+            Alert.alert(
+                'Invalid number!', 'Number has to be a number between 1 and 99.', 
+                [{text: 'Okay', style: 'destructive', onPress: resetInputHandler}])
             return;
         }
         setConfirmed(true);
