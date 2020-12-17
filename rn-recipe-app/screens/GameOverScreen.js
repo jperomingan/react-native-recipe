6518d482ac5afef6d018ce9474d8d5a2c052f6a4
jperomingan@startechup.com <jperomingan@startechup.com>
2020-12-17 18:54:44 +0800

    Adding Custom Fonts

---
+++ rn-recipe-app/screens/GameOverScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import * as Font from 'expo-font';
+
+const GameOverScreen = props => {
+    return (
+        <View style={styles.screen}> 
+            <Text>The Game is Over!</Text>
+            <Text>Number rounds: {props.roundsNumber}</Text>
+            <Text>Number was: {props.userNumber}</Text>
+            <Button title="NEW GAME" onPress={props.onRestart}></Button>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
+
+export default GameOverScreen;
