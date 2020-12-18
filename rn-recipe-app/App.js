984060ba06c36bbfc752133f3724eb91d3dfdc61
jperomingan@startechup.com <jperomingan@startechup.com>
2020-12-18 13:45:29 +0800

    Building a Custom Button Component

--- rn-recipe-app/App.js
+++ rn-recipe-app/App.js
@@ -43,11 +43,12 @@ export default function App() {
   }
 
   let content = <StartGameScreen onStartGame={startGameHandler}/>;
-  content = <GameOverScreen 
+  content = (<GameOverScreen 
   roundsNumber={1}
   userNumber={1}
   onRestart={configureNewGameHandler}
   />
+  );
   if (userNumber && guessRounds <= 0) {
     content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler}/>
   } else if (guessRounds > 0){
