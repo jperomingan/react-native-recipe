8c1105181365c4684e0017ec07274f8e2ba16d9b
jperomingan@startechup.com <jperomingan@startechup.com>
2020-12-18 10:23:24 +0800

    A closer Look at the text component

--- rn-recipe-app/screens/GameOverScreen.js
+++ rn-recipe-app/screens/GameOverScreen.js
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet, Button, Image } from 'react-native';
 
 import BodyText from '../components/BodyText';
 import TitleText from '../components/TitleText';
+import Colors from '../constants/colors';
 
 const GameOverScreen = props => {
     return (
@@ -10,14 +11,26 @@ const GameOverScreen = props => {
             <TitleText>The Game is Over!</TitleText>
             <View style={styles.imageContainer}>
                 <Image 
-                // source={require('../assets/success.png')} 
-                source={{uri: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fhgtvhome.sndimg.com%2Fcontent%2Fdam%2Fimages%2Fhgtv%2Ffullset%2F2015%2F11%2F10%2F0%2FCI_Costa-Farms-Ballad-aster.jpg.rend.hgtvcom.966.644.suffix%2F1447169929799.jpeg&imgrefurl=https%3A%2F%2Fwww.hgtv.com%2Foutdoors%2Fflowers-and-plants%2Fbirth-month-flowers-pictures&tbnid=LtSENaAd7wXk-M&vet=12ahUKEwjZzsDiq9btAhUVDZQKHaueDY0QMygFegUIARDZAQ..i&docid=A3mfmOt66FKeyM&w=966&h=644&q=flowers&ved=2ahUKEwjZzsDiq9btAhUVDZQKHaueDY0QMygFegUIARDZAQ'}} 
+                source={require('../assets/success.png')} 
+                // source={{
+                //     uri: 
+                //     'https://www.google.com/imgres?imgurl=https%3A%2F%2Fhgtvhome.sndimg.com%2Fcontent%2Fdam%2Fimages%2Fhgtv%2Ffullset%2F2015%2F11%2F10%2F0%2FCI_Costa-Farms-Ballad-aster.jpg.rend.hgtvcom.966.644.suffix%2F1447169929799.jpeg&imgrefurl=https%3A%2F%2Fwww.hgtv.com%2Foutdoors%2Fflowers-and-plants%2Fbirth-month-flowers-pictures&tbnid=LtSENaAd7wXk-M&vet=12ahUKEwjZzsDiq9btAhUVDZQKHaueDY0QMygFegUIARDZAQ..i&docid=A3mfmOt66FKeyM&w=966&h=644&q=flowers&ved=2ahUKEwjZzsDiq9btAhUVDZQKHaueDY0QMygFegUIARDZAQ'
+                // }} 
                 style={styles.image}
                 resizeMode="cover"
                 />
             </View>
-            <BodyText>Number rounds: {props.roundsNumber}</BodyText>
-            <BodyText>Number was: {props.userNumber}</BodyText>
+            <View style={styles.resultContainer}>
+                <BodyText style={styles.resultText}>
+                    Your phone needed 
+                    <Text style={styles.highlight}>{props.roundsNumber}
+                    </Text> 
+                    rounds to guess the number 
+                    <Text style={styles.highlight}>{props.userNumber}
+                    </Text>.
+                </BodyText>
+            </View>
+    
             <Button title="NEW GAME" onPress={props.onRestart}></Button>
         </View>
     )
@@ -27,7 +40,8 @@ const styles = StyleSheet.create({
     screen: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        fontFamily
     },
     imageContainer: {
         width: 300,
@@ -41,6 +55,18 @@ const styles = StyleSheet.create({
     image: {
         width: '100%',
         height: '100%'
+    },
+    resultContainer: {
+        marginHorizontal: 30,
+        marginVertical: 15
+    },
+    resultText: {
+        textAlign: 'center',
+        fontSize: 20
+    },
+    highlight: {
+        color: Colors.primary,
+        fontFamily: 'open-sans-bold'
     }
 });
 
