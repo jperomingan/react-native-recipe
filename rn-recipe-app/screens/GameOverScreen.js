c7be600486b994f556237ffb30d98e95de1b84e8
jperomingan@startechup.com <jperomingan@startechup.com>
2020-12-18 09:13:26 +0800

    Working with Network (Web) Images

--- rn-recipe-app/screens/GameOverScreen.js
+++ rn-recipe-app/screens/GameOverScreen.js
@@ -10,7 +10,8 @@ const GameOverScreen = props => {
             <TitleText>The Game is Over!</TitleText>
             <View style={styles.imageContainer}>
                 <Image 
-                source={require('../assets/success.png')} 
+                // source={require('../assets/success.png')} 
+                source={{uri: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fhgtvhome.sndimg.com%2Fcontent%2Fdam%2Fimages%2Fhgtv%2Ffullset%2F2015%2F11%2F10%2F0%2FCI_Costa-Farms-Ballad-aster.jpg.rend.hgtvcom.966.644.suffix%2F1447169929799.jpeg&imgrefurl=https%3A%2F%2Fwww.hgtv.com%2Foutdoors%2Fflowers-and-plants%2Fbirth-month-flowers-pictures&tbnid=LtSENaAd7wXk-M&vet=12ahUKEwjZzsDiq9btAhUVDZQKHaueDY0QMygFegUIARDZAQ..i&docid=A3mfmOt66FKeyM&w=966&h=644&q=flowers&ved=2ahUKEwjZzsDiq9btAhUVDZQKHaueDY0QMygFegUIARDZAQ'}} 
                 style={styles.image}
                 resizeMode="cover"
                 />
