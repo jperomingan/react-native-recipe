76ec830949a3dcf13f635efe57834a0b079f3b6c
jperomingan@startechup.com <jperomingan@startechup.com>
2020-12-21 16:29:58 +0800

    Calculating Sizes Dynamically

--- rn-recipe-app/screens/GameOverScreen.js
+++ rn-recipe-app/screens/GameOverScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, Dimensions, ScrollView } from 'react-native';
 
 import BodyText from '../components/BodyText';
 import TitleText from '../components/TitleText';
@@ -8,32 +8,34 @@ import Colors from '../constants/colors';
 
 const GameOverScreen = props => {
     return (
-        <View style={styles.screen}> 
-            <TitleText>The Game is Over!</TitleText>
-            <View style={styles.imageContainer}>
-                <Image 
-                source={require('../assets/success.png')} 
-                // source={{
-                //     uri: 
-                //     'https://www.google.com/imgres?imgurl=https%3A%2F%2Fhgtvhome.sndimg.com%2Fcontent%2Fdam%2Fimages%2Fhgtv%2Ffullset%2F2015%2F11%2F10%2F0%2FCI_Costa-Farms-Ballad-aster.jpg.rend.hgtvcom.966.644.suffix%2F1447169929799.jpeg&imgrefurl=https%3A%2F%2Fwww.hgtv.com%2Foutdoors%2Fflowers-and-plants%2Fbirth-month-flowers-pictures&tbnid=LtSENaAd7wXk-M&vet=12ahUKEwjZzsDiq9btAhUVDZQKHaueDY0QMygFegUIARDZAQ..i&docid=A3mfmOt66FKeyM&w=966&h=644&q=flowers&ved=2ahUKEwjZzsDiq9btAhUVDZQKHaueDY0QMygFegUIARDZAQ'
-                // }} 
-                style={styles.image}
-                resizeMode="cover"
-                />
+        <ScrollView>
+            <View style={styles.screen}> 
+                <TitleText>The Game is Over!</TitleText>
+                <View style={styles.imageContainer}>
+                    <Image 
+                    source={require('../assets/success.png')} 
+                    // source={{
+                    //     uri: 
+                    //     'https://www.google.com/imgres?imgurl=https%3A%2F%2Fhgtvhome.sndimg.com%2Fcontent%2Fdam%2Fimages%2Fhgtv%2Ffullset%2F2015%2F11%2F10%2F0%2FCI_Costa-Farms-Ballad-aster.jpg.rend.hgtvcom.966.644.suffix%2F1447169929799.jpeg&imgrefurl=https%3A%2F%2Fwww.hgtv.com%2Foutdoors%2Fflowers-and-plants%2Fbirth-month-flowers-pictures&tbnid=LtSENaAd7wXk-M&vet=12ahUKEwjZzsDiq9btAhUVDZQKHaueDY0QMygFegUIARDZAQ..i&docid=A3mfmOt66FKeyM&w=966&h=644&q=flowers&ved=2ahUKEwjZzsDiq9btAhUVDZQKHaueDY0QMygFegUIARDZAQ'
+                    // }} 
+                    style={styles.image}
+                    resizeMode="cover"
+                    />
+                </View>
+                <View style={styles.resultContainer}>
+                    <BodyText style={styles.resultText}>
+                        Your phone needed 
+                        <Text style={styles.highlight}>{props.roundsNumber}
+                        </Text> 
+                        rounds to guess the number 
+                        <Text style={styles.highlight}>{props.userNumber}
+                        </Text>.
+                    </BodyText>
+                </View>
+        
+                <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
             </View>
-            <View style={styles.resultContainer}>
-                <BodyText style={styles.resultText}>
-                    Your phone needed 
-                    <Text style={styles.highlight}>{props.roundsNumber}
-                    </Text> 
-                    rounds to guess the number 
-                    <Text style={styles.highlight}>{props.userNumber}
-                    </Text>.
-                </BodyText>
-            </View>
-    
-            <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
-        </View>
+        </ScrollView>
     )
 };
 
@@ -44,13 +46,13 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     imageContainer: {
-        width: 300,
-        height: 300,
-        borderRadius: 150,
+        width: Dimensions.get('window').width * 0.7,
+        height: Dimensions.get('window').width * 0.7,
+        borderRadius: Dimensions.get('window').width * 0.7 / 2,
         borderWidth: 3,
         borderColor: 'black',
         overflow: 'hidden',
-        marginVertical: 30
+        marginVertical: Dimensions.get('window').height / 30
     },
     image: {
         width: '100%',
@@ -58,11 +60,11 @@ const styles = StyleSheet.create({
     },
     resultContainer: {
         marginHorizontal: 30,
-        marginVertical: 15
+        marginVertical: Dimensions.get('window').height / 60
     },
     resultText: {
         textAlign: 'center',
-        fontSize: 20
+        fontSize: Dimensions.get('window').height < 400 ? 16 : 20
     },
     highlight: {
         color: Colors.primary,
--- rn-recipe-app/screens/GameScreen.js
+++ rn-recipe-app/screens/GameScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, StyleSheet, Alert, ScrollView, FlatList } from 'react-native';
+import { View, Text, StyleSheet, Alert, ScrollView, FlatList, Dimensions } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 import NumberContainer from '../components/NumberContainer';
@@ -58,7 +58,12 @@ const GameScreen = props => {
             // setRounds(curRounds => curRounds + 1);
             setPastGuesses(curPastGuesses => [nextNumber.toString(), ...curPastGuesses]);
         };
-        
+
+        let listContainerStyle = styles.listContainer;
+        if (Dimensions.get('window').width < 350) {
+            listContainerStyle = styles.listContainerBig;
+        }
+
         return (
             <View style={styles.screen}>
                 <Text style={DefaultStyles.title}>Opponent's Guess</Text>
@@ -95,13 +100,17 @@ const styles = StyleSheet.create({
     buttonContainer: {
         flexDirection: 'row',
         justifyContent: 'space-around',
-        marginTop: 20,
+        marginTop: Dimensions.get('window').height > 350 ? 20 : 5,
         width: 400,
         maxWidth: '90%'
     },
     listContainer: {
         flex: 1,
         width: '60%'
+    },
+    listContainerBig: {
+        flex: 1,
+        width: '80%'
     },
     list: {
         flexGrow: 1,
