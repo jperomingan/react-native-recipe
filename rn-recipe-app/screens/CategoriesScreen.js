aaf9542dcb70340d1c3878a1cf5e5988665d99bf
jperomingan@startechup.com <jperomingan@startechup.com>
2020-12-23 17:09:18 +0800

    Outputting a Grid of Categories

--- rn-recipe-app/screens/CategoriesScreen.js
+++ rn-recipe-app/screens/CategoriesScreen.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data';
+
+const renderGridItem = (itemData) => {
+    return (
+        <View style={styles.gridItem}>
+            <Text>{itemData.item.title}</Text>
+        </View>
+    );
+}
 
 const CategoriesScreen = props => {
+    console.log(props);
     return (
-        <View style={styles.screen}>
-            <Text>The Categories Screen!</Text>
-        </View>
+        <FlatList keyExtractor={(item, index) => item.id} data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
     );
 };
 
@@ -14,6 +23,11 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    gridItem: {
+        flex: 1,
+        margin: 15,
+        height: 150
     }
 }); 
 
