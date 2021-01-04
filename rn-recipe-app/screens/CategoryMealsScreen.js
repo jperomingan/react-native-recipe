01ad12572a3b44fae46ecd9b58442cae652290d6
jperomingan@startechup.com <jperomingan@startechup.com>
2021-01-04 15:14:21 +0800

    Setting Dynamic Navigation Options

--- rn-recipe-app/screens/CategoryMealsScreen.js
+++ rn-recipe-app/screens/CategoryMealsScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, Platform } from 'react-native';
 
 import { CATEGORIES } from '../data/dummy-data';
+import Colors from '../constants/Colors';
 
 const CategoryMealsScreen = props => {
-    const catId = props.navigation.getParams('categoryId');
+    const catId = props.navigation.getParam('categoryId');
     const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
     return (
         <View style={styles.screen}>
@@ -18,6 +19,20 @@ const CategoryMealsScreen = props => {
             }}/>
         </View>
     );
+};
+
+CategoryMealsScreen.navigationOptions = navigationData => {
+    const catId = navigationData.navigation.getParam('categoryId');
+
+    const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
+    return {
+        headerTitle: selectedCategory.title,
+        headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? Colors.primaryColor: ''
+        },
+        headerTinColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+    };
 };
 
 const styles = StyleSheet.create({
