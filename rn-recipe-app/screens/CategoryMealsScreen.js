e49a206eb63d9cbbbaf5cb700e6a2c4598abf8cb
jperomingan@startechup.com <jperomingan@startechup.com>
2021-01-05 15:15:57 +0800

    Rendering a Meals List

--- rn-recipe-app/screens/CategoryMealsScreen.js
+++ rn-recipe-app/screens/CategoryMealsScreen.js
@@ -6,7 +6,14 @@ import MealItem from '../components/MealItem';
 
 const CategoryMealsScreen = props => {
     const renderMealItem = itemData => {
-        return <MealItem title={itemData.item.title} onSelectMeal={() => {}}/>;
+        return <MealItem 
+        title={itemData.item.title} 
+        image={itemData.item.imageUrl}
+        duration={itemData.item.duration} 
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+        onSelectMeal={() => {}}
+        />;
     }
     const catId = props.navigation.getParam('categoryId');
     const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
@@ -16,6 +23,7 @@ const CategoryMealsScreen = props => {
            data={displayedMeals} 
            keyExtractor={(item, index) => item.id} 
            renderItem={renderMealItem} 
+           style={{width: '100%'}}
            />
         </View>
     );
