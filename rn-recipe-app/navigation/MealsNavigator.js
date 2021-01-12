9bac4b3b8d0a36d6917ab40beeae67e6a5d0cb3a
jperomingan@startechup.com <jperomingan@startechup.com>
2021-01-12 20:19:18 +0800

    Adding a DefaultText Component

--- rn-recipe-app/navigation/MealsNavigator.js
+++ rn-recipe-app/navigation/MealsNavigator.js
@@ -1,6 +1,7 @@
-import { Platform } from 'react-native';
+import { Platform, Text } from 'react-native';
 import React from 'react';
-import { createAppContainer } from 'react-navigation';
+import { createAppContainer} from 'react-navigation';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { Ionicons } from '@expo/vector-icons';
@@ -10,11 +11,18 @@ import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
 import FavoritesScreen from '../screens/FavoriteScreen';
+import FiltersScreen from '../screens/FiltersScreen';
 import Colors from '../constants/Colors';
 
 const defaultStackNavOptions = {
     headerStyle: {
       backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+    },
+    headerTitleStyle: {
+      fontFamily: 'open-sans-bold'
+    },
+    headerBackTitleStyle: {
+        fontFamily: 'open-sans'
     },
     headerTintColor:
       Platform.OS === 'android' ? 'white' : Colors.primaryColor,
@@ -52,7 +60,9 @@ const tabScreenConfig = {
           <Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor} />
         );
       },
-      tabBarColor: Colors.primaryColor
+      tabBarColor: Colors.primaryColor,
+      tabBarLabel: Platform.OS === 'android' ? <Text style={{fontFamily: 'open-sans-bold'}}>Meals</Text>:
+      'Meals'
     }
   },
   Favorites: {
@@ -61,7 +71,9 @@ const tabScreenConfig = {
       tabBarIcon: tabInfo => {
         return <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />;
       },
-      tabBarColor: Colors.accentColor
+      tabBarColor: Colors.accentColor,
+      tabBarLabel: Platform.OS === 'android' ? <Text style={{fontFamily: 'open-sans-bold'}}>Favorites</Text>:
+      'Favorites'
     }
   }
 };
@@ -77,9 +89,35 @@ const MealsFavTabNavigator =
       })
     : createBottomTabNavigator(tabScreenConfig, {
         tabBarOptions: {
+          labelStyle: {
+            fontFamily: 'open-sans-bold'
+          },
           activeTintColor: Colors.accentColor
         }
       });
 
-export default createAppContainer(MealsFavTabNavigator);
+  const FiltersNavigator = createStackNavigator({
+      Filters: FiltersScreen
+  }, {
+    // navigationOptions: {
+    //   drawerLabel: 'Filters!!!!'
+    // },
+    defaultNavigationOptions: defaultStackNavOptions
+  })
+
+  const MainNavigator = createDrawerNavigator({
+    MealsFavs: {screen: MealsFavTabNavigator, navigationOptions: {
+      drawerLabel: 'Meals'
+    }},
+    Filters: FiltersNavigator
+  }, {
+    contentOptions: {
+      activeTintColor: Colors.accentColor,
+      labelStyle: {
+        fontFamily: 'open-sans-bold'
+      }
+    }
+  });
+
+export default createAppContainer(MainNavigator);
 
--- rn-recipe-app/screens/CategoriesScreen.js
+++ rn-recipe-app/screens/CategoriesScreen.js
@@ -6,9 +6,10 @@ import {
     StyleSheet, 
     TouchableOpacity,
 } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
+import HeaderButton from '../components/HeaderButton';
 import { CATEGORIES } from '../data/dummy-data';
-import Colors from '../constants/Colors';
 import CategoryGridTile from '../components/CategoryGridTile';
 
 const CategoriesScreen = props => {
@@ -27,8 +28,20 @@ const CategoriesScreen = props => {
     );
 };
 
-CategoriesScreen.navigationOptions = {
-    headerTitle: 'Meal Categories',
+CategoriesScreen.navigationOptions = navData => {
+    return {
+        headerTitle: 'Meal Categories',
+        headerLeft: (
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item 
+                title="Menu" 
+                iconName="ios-menu" 
+                onPress={() => {
+                    navData.navigation.toggleDrawer()
+                }} />
+            </HeaderButtons>
+        )
+    } 
 };
 
 const styles = StyleSheet.create({
